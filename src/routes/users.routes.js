9219bf324bd8fd826d23c25f4bb157a1e7974b35
Pedro Kaneto <Pedro.Kaneto@ibratecbrasil>
2023-08-14 16:27:58 -0300

    finish api rest

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,12 +1,20 @@
 const { Router } = require("express");
+const multer = require("multer");
+const uploadsConfig = require("../configs/upload")
 
 const UsersController = require("../controllers/UsersController");
+const UserAvatarController = require("../controllers/UserAvatarController");
+const ensureAuthenticated  = require("../middlewares/ensureAuthenticated");
+
 
 const usersRoutes = Router();
+const upload = multer(uploadsConfig.MULTER)
 
 const usersController = new UsersController();
+const userAvatarController = new UserAvatarController();
 
 usersRoutes.post("/", usersController.create);
-usersRoutes.put("/:id", usersController.update);
+usersRoutes.put("/", ensureAuthenticated, usersController.update);
+usersRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), userAvatarController.update)
 
 module.exports = usersRoutes;
